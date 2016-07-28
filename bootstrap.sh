22c5c1b9abf3183cb133170be41f4992a98b62e9
Nicholas Chaimov <nicholas.a.chaimov@intel.com>
2016-07-28 14:31:44 -0700

    Basic CMake setup and hello-world program.

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+           
+export BOOST_ROOT=$HOME/hpx/boost_1_57_0
+export OTF2_ROOT=$HOME/hpx/otf2-2.0-inst
+
+SOURCE_DIR=$PWD
+BUILD_DIR=$PWD/build
+INST_DIR=$PWD/build
+
+mkdir -p build
+cd $BUILD_DIR
+cmake $SOURCE_DIR -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$INST_DIR -DBOOST_ROOT=$BOOST_ROOT -DOTF2_ROOT=$OFT2_ROOT
+make
+make install
+cd $SOURCE_DIR
